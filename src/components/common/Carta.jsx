0d766ab6cbb199678ed93d3125ab830f64c3d2ef
felipegtrujillo <felipegtrujillo@gmail.com>
2024-10-15 21:25:32 -0300

    cambios menu

--- src/components/common/Carta.jsx
+++ src/components/common/Carta.jsx
@@ -7,6 +7,7 @@ import img1 from '../../../public/assets/img/restaurant/bar.jpg';
 
 const Carta = () => {
   const pdfUrl = 'https://firebasestorage.googleapis.com/v0/b/lacalchona-cd046.appspot.com/o/CARTA-LA-CALCHONA.pdf?alt=media&token=b5fef067-ce63-4481-a0e7-95b5c7bbd35f';
+  const googleDocsViewerUrl = `https://docs.google.com/gview?url=${encodeURIComponent(pdfUrl)}&embedded=true`;
 
   return (
     <div id="cartaPDF" className=" relative flex items-center justify-center bg-gray-800 min-w-screen min-h-screen  ">
@@ -18,7 +19,7 @@ const Carta = () => {
 
           <div className='absolute flex items-center justify-center w-full h-full  '>
           <iframe
-            src={`${pdfUrl}#page=2`}
+            src={googleDocsViewerUrl}
 
             style={{ border: 'none' }}
             title="PDF Viewer"
