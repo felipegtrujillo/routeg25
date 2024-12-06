2594c6e0c48efb1fef7508a09c8a5ed79c8054fd
felipegtrujillo <felipegtrujillo@gmail.com>
2024-12-06 11:06:00 -0300

    gracias

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,21 +1,23 @@
-import './globals.css';
+import "./globals.css";
 
-import { montserrat } from '../app/ui/fonts';
+import { montserrat } from "../app/ui/fonts";
 
 export const metadata = {
   title: "La Calchona",
-  description: "Restaurant, hospedaje, alojamiento y cabañas en el Cajón del Maipo",
+  description:
+    "Restaurant, hospedaje, alojamiento y cabañas en el Cajón del Maipo",
   icons: {
     icon: "/lacalchona.svg", // Define el ícono aquí
   },
   openGraph: {
     title: "La Calchona",
-    description: "Restaurant, hospedaje, alojamiento y cabañas en el Cajón del Maipo",
+    description:
+      "Restaurant, hospedaje, alojamiento y cabañas en el Cajón del Maipo",
     url: "https://lacalchona.cl",
     siteName: "La Calchona",
     images: [
       {
-        url: "/lacalchona.png", // Imagen para compartir
+        url: "/lacalchona1.png", // Imagen para compartir
         width: 1200,
         height: 630,
         alt: "La Calchona - Cajón del Maipo",
@@ -29,15 +31,13 @@ export default function RootLayout({ children }) {
   return (
     <html className="relative" lang="en">
       <head>
-      <link rel="icon" type="image/svg+xml" href="/lacalchona.svg" /> 
+        <link rel="icon" type="image/svg+xml" href="/lacalchona.svg" />
       </head>
       <body className={`${montserrat.className} antialiased`}>
-              {/* El contenido que se animará está envuelto en un componente cliente */}
- 
-                {children}
+        {/* El contenido que se animará está envuelto en un componente cliente */}
 
+        {children}
       </body>
-
     </html>
   );
 }
